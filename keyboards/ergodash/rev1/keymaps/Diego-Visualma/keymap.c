3f097fe1bb6556fcefb1d0fb1dc2747308a04097
Diego-Visualma <mora@visualma.com>
2020-08-15 06:58:35 -0500

    temporary switch to querty using tap dance

--- keyboards/ergodash/rev1/keymaps/Diego-Visualma/keymap.c
+++ keyboards/ergodash/rev1/keymaps/Diego-Visualma/keymap.c
@@ -1,8 +1,7 @@
 #include QMK_KEYBOARD_H
 
-
-#define _QWERTY 0
-#define _COLEMAKMD 1
+#define _COLEMAKMD 0
+#define _QWERTY 1
 #define _LOWER 2
 #define _RAISE 3
 #define _ADJUST 4
@@ -19,14 +18,35 @@ enum custom_keycodes {
 };
 
 //Tap Dance declarations
+typedef struct {
+	bool is_press_action;
+	uint8_t state;
+} tap;
+
 enum {
-	TD_EXPN,
+	SINGLE_TAP = 1,
+	SINGLE_HOLD,
+	DOUBLE_TAP,
+	DOUBLE_HOLD,
+	DOUBLE_SINGLE_TAP,
+	TRIPLE_TAP,
+	TRIPLE_HOLD,
+};
+
+enum {
+	TD_LOWER,
+  TD_RAISE,
+	TD_SHCPS,
 };
 
 //Tap Dance definitios
-qk_tap_dance_action_t tap_dance_actions[] = {
-	[TD_EXPN] = ACTION_TAP_DANCE_DOUBLE(KC_N, ENIE),
-};
+uint8_t cur_dance(qk_tap_dance_state_t *state);
+void lower_finished(qk_tap_dance_state_t *state, void *user_data);
+void lower_reset(qk_tap_dance_state_t *state, void *user_data);
+void raise_finished(qk_tap_dance_state_t *state, void *user_data);
+void raise_reset(qk_tap_dance_state_t *state, void *user_data);
+void shcps_finished(qk_tap_dance_state_t *state, void *user_data);
+void shcps_reset(qk_tap_dance_state_t *state, void *user_data);
 
 #define EISU LALT(KC_GRV)
 #define SHIFTAB LSFT(KC_TAB)
@@ -55,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    NUMPAD, \
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_BSPC,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    OSSTFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_ENT ,      KC_SPC ,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,          LOWER,   KC_SPC ,KC_LCTL,      KC_RCTL,  KC_ENT , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    TD(TD_SHCPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_ENT ,      KC_SPC ,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,        TD(TD_LOWER),   KC_SPC ,KC_LCTL,      KC_RCTL,  KC_ENT , TD(TD_RAISE),            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
     /* Colemak
@@ -76,8 +96,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC  ,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,                        KC_RBRC, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    NUMPAD, \
     KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_MINS,                        KC_EQL , KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_BSLS, \
     KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_BSPC,                        KC_BSPC, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-    OSSTFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,            KC_ENT ,      KC_SPC ,           KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,          LOWER,   KC_SPC, KC_LCTL,      KC_RCTL,  KC_ENT , RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    TD(TD_SHCPS), KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,            KC_ENT ,      KC_SPC ,           KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,         TD(TD_LOWER),   KC_SPC, KC_LCTL,      KC_RCTL,  KC_ENT , TD(TD_RAISE),            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
   /* Lower
@@ -98,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ENIE   , KC_DQT , \
     OSSTFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MSTP ,        KC_MNXT ,        ENIE,    KC_M,    KC_LT,   KC_GT,   KC_MINS, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,          LOWER,   KC_SPC, KC_MPLY,        KC_MPRV, KC_ENT, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,          TD(TD_LOWER),   KC_SPC, KC_MPLY,        KC_MPRV, KC_ENT, TD(TD_RAISE),            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
   /* Raise
@@ -119,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL ,                        KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ENIE   , KC_DQT , \
     OSSTFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MSTP ,        KC_MNXT ,        ENIE,    KC_M,    KC_LT,   KC_GT,   KC_MINS, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,          LOWER,   KC_SPC, KC_MPLY,        KC_MPRV, KC_ENT, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR,          TD(TD_LOWER),   KC_SPC, KC_MPLY,        KC_MPRV, KC_ENT, TD(TD_RAISE),            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
     /* Numpad
@@ -178,11 +198,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-         print("mode just switched to qwerty and this is a huge string\n");
+        print("mode just switched to qwerty and this is a huge string\n");
+	      layer_move(_QWERTY);
         set_single_persistent_default_layer(_QWERTY);
-	rgblight_mode(RGBLIGHT_MODE_SNAKE);
       }
-      return false;
+      return 
+      false;
       break;
     case LOWER:
       if (record->event.pressed) {
@@ -215,11 +236,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case NUMPAD:
       if (record->event.pressed) {
           layer_invert(_NUMPAD);
-	  if( layer_state_is(_NUMPAD) ){
-		  rgblight_mode(RGBLIGHT_MODE_KNIGHT);
-	  }else{
-		  rgblight_mode(RGBLIGHT_MODE_STATIC_GRADIENT);
-	  }
 	}
     break;
     case ENIE:
@@ -237,10 +253,161 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     break;
     case COLEMK:
       if (record->event.pressed) {
+	layer_move(_COLEMAKMD);      
         set_single_persistent_default_layer(_COLEMAKMD);
-	rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
       } 
     break;
   }
   return true;
 }
+
+// Change lights based on layer state via callback
+layer_state_t layer_state_set_user(layer_state_t state){
+	switch(biton32(state)){
+		case _QWERTY:
+			rgblight_mode(RGBLIGHT_MODE_BREATHING);
+			rgblight_sethsv_noeeprom(HSV_ORANGE);
+			break;
+		case _COLEMAKMD:
+			rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+			rgblight_sethsv(100,230,110);
+			break;
+		case _NUMPAD:
+			rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
+			rgblight_sethsv_noeeprom(HSV_GOLD);
+			break;
+		case _LOWER:
+			//rgblight_mode();
+			break;
+		case _RAISE:
+			//rgblight_mode();
+			break;
+		case _ADJUST:
+			rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
+			rgblight_sethsv_noeeprom(HSV_RED);
+			break;
+		default:
+			rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+			rgblight_sethsv_noeeprom(HSV_CYAN);
+			break;
+	}
+	return state;
+}
+
+//multi tap dance
+uint8_t cur_dance(qk_tap_dance_state_t *state) {
+	if (state->count == 1) {
+		if (state->interrupted || state->pressed) return SINGLE_HOLD;
+		else return SINGLE_TAP;
+	} else if (state->count == 2) {
+		if (state->interrupted) return DOUBLE_SINGLE_TAP;
+		else if (state->pressed) return DOUBLE_HOLD;
+		else return DOUBLE_TAP;
+	}
+	if (state->count == 3) {
+		if (state->interrupted || !state->pressed) return TRIPLE_TAP;
+		else return TRIPLE_HOLD;
+	} else return 8;
+}
+//create instances of tap for each key
+static tap lr_tap_state = {
+	.is_press_action = true,
+	.state = 0
+};
+static tap ra_tap_state = {
+	.is_press_action = true,
+	.state = 0
+};
+static tap sh_tap_state = {
+	.is_press_action = true,
+	.state = 0
+};
+
+void lower_finished(qk_tap_dance_state_t *state, void *user_data) {
+	lr_tap_state.state = cur_dance(state);
+	switch (lr_tap_state.state){
+		case SINGLE_TAP: register_code(KC_LBRC); break;
+		case SINGLE_HOLD: 
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    break;
+		case DOUBLE_TAP: break;
+		case DOUBLE_HOLD:
+        layer_on(_QWERTY);
+    break;
+		case DOUBLE_SINGLE_TAP: break;
+	}
+}
+void lower_reset(qk_tap_dance_state_t *state, void *user_data) {
+	switch (lr_tap_state.state){
+		case SINGLE_TAP: unregister_code(KC_LBRC); break;
+		case SINGLE_HOLD: 
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    break;
+		case DOUBLE_TAP: break;
+		case DOUBLE_HOLD: 
+        layer_off(_QWERTY);
+    break;
+		case DOUBLE_SINGLE_TAP: break;
+	}
+  lr_tap_state.state = 0;
+}
+
+void raise_finished(qk_tap_dance_state_t *state, void *user_data) {
+	ra_tap_state.state = cur_dance(state);
+	switch (ra_tap_state.state){
+		case SINGLE_TAP: register_code(KC_RBRC); break;
+		case SINGLE_HOLD: 
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    break;
+		case DOUBLE_TAP: break;
+		case DOUBLE_HOLD:
+        layer_on(_QWERTY);
+    break;
+		case DOUBLE_SINGLE_TAP: break;
+	}
+}
+void raise_reset(qk_tap_dance_state_t *state, void *user_data) {
+	switch (ra_tap_state.state){
+		case SINGLE_TAP: unregister_code(KC_RBRC); break;
+		case SINGLE_HOLD: 
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    break;
+		case DOUBLE_TAP: break;
+		case DOUBLE_HOLD: 
+        layer_off(_QWERTY);
+    break;
+		case DOUBLE_SINGLE_TAP: break;
+	}
+  ra_tap_state.state = 0;
+}
+
+void shcps_finished(qk_tap_dance_state_t *state, void *user_data) {
+	sh_tap_state.state = cur_dance(state);
+	switch (sh_tap_state.state){
+		case SINGLE_TAP: register_code(KC_LSFT); break;
+		case SINGLE_HOLD: register_code(KC_LSFT); break;
+		case DOUBLE_TAP: register_code(KC_CAPSLOCK); break;
+		case DOUBLE_HOLD: register_code(KC_LSFT); break;
+		case DOUBLE_SINGLE_TAP: break;
+	}
+}
+void shcps_reset(qk_tap_dance_state_t *state, void *user_data) {
+	switch (sh_tap_state.state){
+		case SINGLE_TAP: unregister_code(KC_LSFT); break;
+		case SINGLE_HOLD: unregister_code(KC_LSFT); break;
+		case DOUBLE_TAP: unregister_code16(KC_CAPSLOCK); break;
+		case DOUBLE_HOLD:unregister_code(KC_LSFT); break;
+		case DOUBLE_SINGLE_TAP: break;
+	}
+  sh_tap_state.state = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+	[TD_LOWER] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, lower_finished, lower_reset, 215),
+  [TD_RAISE] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, raise_finished, raise_reset, 215),
+	[TD_SHCPS] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, shcps_finished, shcps_reset, 215),
+};
